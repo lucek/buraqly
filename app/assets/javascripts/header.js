e64dcd2c5e1bea08ecf5ef4f0296e715d17e6c0e
lucek <lukasz.odziewa@llp.pl>
2014-06-06 15:09:28 +0200

    header

--- app/assets/javascripts/header.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $(".header-top").mouseenter(function() {
-      $(".header-top").fadeTo("slow", 1);
-    }).mouseout(function() {
-      $(".header-top").fadeTo("slow", 0);
-    })
-});
